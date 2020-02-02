06704b69c2cbefd835ded9a4347b59b0ec9d2905
b.berg15618 <b.berg15618@hs-ansbach.de>
2020-02-02 13:20:09 +0100

    its a trap

--- app/src/main/java/com/project/little_collectipair/MainActivity.java
+++ app/src/main/java/com/project/little_collectipair/MainActivity.java
@@ -159,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
     private void RenderableModel_LVL1(Anchor anchor)
     {
         int count = 0;
-        String[] sfbfiles = {"reifen.sfb", "stern.sfb", "autokarosse.sfb", "autogesamt.sfb"};
+        String[] sfbfiles = {"reifen.sfb", "mercedesstern.sfb", "autokarosse.sfb", "autogesamt.sfb"};
 
         if(item_count < 4)  { count = 0; }
         if(item_count <6 && item_count >= 4) { count = 1; }
@@ -288,9 +288,10 @@ public class MainActivity extends AppCompatActivity {
     private void deletcounter() {
         TextView Ausgabe = findViewById(R.id.itemsCntTxt);
         if (item_count <= items_sum) {
-            new Thread(() -> {
-                runOnUiThread(() -> Ausgabe.setText("Eingesammelte Items " + item_count + "von " + items_sum));
-            }).start();
-        }
+           // new Thread(() -> {
+                //runOnUiThread(() ->
+                        Ausgabe.setText("Eingesammelte Items " + item_count + "von " + items_sum);
+           // }).start();
+       }
     }
 }
