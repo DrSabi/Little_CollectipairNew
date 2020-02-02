8264596d598a52113d56f67e28f810546dfd615a
Avenicc <schmoschu@gmail.com>
2020-02-02 09:23:05 +0100

    random spawns activated + menue wieder eingefuegt

--- app/src/main/java/com/project/little_collectipair/MainActivity.java
+++ app/src/main/java/com/project/little_collectipair/MainActivity.java
@@ -18,113 +18,100 @@ import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
 import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Frame;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.core.Session;
+import com.google.ar.core.TrackingState;
 import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.Camera;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.SkeletonNode;
 import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.Material;
+import com.google.ar.sceneform.rendering.MaterialFactory;
 import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.ShapeFactory;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 import android.os.Build.VERSION_CODES;
 import androidx.fragment.app.FragmentActivity;
 
+import java.util.Collection;
+
 //TEST Denny ist ein plöder pursche und iich haue ihn gleich zu poden
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private static final double MIN_OPENGL_VERSION = 3.0;
     private ArFragment arFragment;
     private ModelRenderable tireRenderable;
-    private static final int RC_permissions = 0x123;
-    private GestureDetector gestureDetector;
-    private boolean hasFinishedLoading = false;
-    private boolean hasPlacedCarParts;
+    private boolean isModelPlaced = false;
+
+
 
     @Override
-    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        if (!checkIsSupportedDeviceOrFinish(this)) {
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+
+        arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdate);
+
+
+    }
+
+    private void onUpdate(FrameTime frameTime) {
+
+        if(isModelPlaced)
             return;
+
+
+        Frame frame = arFragment.getArSceneView().getArFrame();
+
+        Collection<Plane> planes = frame.getUpdatedTrackables(Plane.class);
+
+        System.out.println("test");
+
+        for(Plane plane : planes) {
+
+            if(plane.getTrackingState() == TrackingState.TRACKING){
+
+                Anchor anchor = plane.createAnchor(plane.getCenterPose());
+
+                makeCube(anchor);
+
+                break;
+            }
+
         }
-        setContentView(R.layout.activity_ux);
-        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+    }
+
+    private void makeCube(Anchor anchor) {
+
+        isModelPlaced = true;
+
+
 
         ModelRenderable.builder()
-                .setSource(this, R.raw.reifen)
+               .setSource(this, Uri.parse("reifen.sfb"))
                 .build()
-                .thenAccept(renderable -> tireRenderable = renderable)
-                .exceptionally(
-                        throwable -> {
-                            Toast toast =
-                                    Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
-                            toast.setGravity(Gravity.CENTER, 0, 0);
-                            toast.show();
-                            return null;
-                        });
-        arFragment.setOnTapArPlaneListener(
-                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
-                    if (tireRenderable == null) {
-                        return;
-                    }
+                .thenAccept(tireRenderable -> {;
 
-                    Anchor anchor = hitResult.createAnchor();
+
                     AnchorNode anchorNode = new AnchorNode(anchor);
-                    anchorNode.setParent(arFragment.getArSceneView().getScene());
-                    TransformableNode tire = new TransformableNode(arFragment.getTransformationSystem());
-                    tire.setParent(anchorNode);
-                    tire.setRenderable(tireRenderable);
-                    tire.select();
+
+                    SkeletonNode skeletonNode = new SkeletonNode();
+                    skeletonNode.setParent(anchorNode);
+                    skeletonNode.setRenderable(tireRenderable);
+
+
+                    arFragment.getArSceneView().getScene().addChild(anchorNode);
                 });
-    }
 
 
-    // Set to true ensures requestInstall() triggers installation if necessary.
-    private boolean mUserRequestedInstall = true;
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // ARCore requires camera permmission to operate.
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            CameraPermissionHelper.requestCameraPermission(this);
-            return;
-        }
-    }
-
-    @Override public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
-                    .show();
-            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
-                CameraPermissionHelper.launchPermissionSettings(this);
-            }
-            finish();
-        }
-    }
-    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-            Log.e(TAG, "Sceneform requires Android N or later");
-            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
-            activity.finish();
-            return false;
-        }
-        String openGlVersionString =
-                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
-                        .getDeviceConfigurationInfo()
-                        .getGlEsVersion();
-        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
-            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
-            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
-                    .show();
-            activity.finish();
-            return false;
-        }
-        return true;
     }
 }
-
